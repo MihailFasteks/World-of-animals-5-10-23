4e1870f4b3c4777f0c6a4b6a16f6caa308dde43f
MihailFasteks <fastakovskiy.mihail04@gmail.com>
2023-12-16 13:07:59 +0200

    Initial Commit

---
+++ World of animals/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  World of animals
+//
+//  Created by Michalis on 16.12.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
